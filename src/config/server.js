c503d47dbfaf3b58c2b7aacd90496cf92338badf
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-08 13:54:09 -0300

    finalização back end

--- src/config/server.js
+++ src/config/server.js
@@ -3,9 +3,13 @@ const port = 3003
 const bodyParse = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParse.urlencoded({extended: true}))
 server.use(bodyParse.json())
+server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`Back end está rodando na porta ${port}.`)
